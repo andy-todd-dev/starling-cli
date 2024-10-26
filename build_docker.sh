23f64c62f35333e2a98b5fb252a772f0d5c36e0d
Andrew Todd <emailandytodd@gmail.com>
2024-10-26 11:09:05 +0000

    Allow dev builds

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+if [ "$2" == "-d" ]; then
+    VER="$1-d"
+else 
+    VER="$1"
+fi
+
 docker build . -f ./docker/Dockerfile -t thegeektechworkshop/starling-cli:$1 -t thegeektechworkshop/starling-cli:latest --push --build-arg STARLING_VERSION=$1
